483a52beaa37ba2beed2ca5cc63a85295adb456b
AtharvaSachan123 <atharvasachan823@gmail.com>
2023-10-18 23:01:33 +0530

    Added keypress

--- index.js
+++ index.js
@@ -1,30 +1,41 @@
-document.querySelector(".bobby").onclick = temperature;
-var Re=document.querySelector(".Reset");
+var cli = document.querySelector(".bobby");
+cli.onclick = temperature;
+var Re = document.querySelector(".Reset");
+
+// Function to perform temperature conversion
 function temperature() {
     var kel = document.getElementById("kel").value;
     var cel = document.getElementById("Celi").value;
     var far = document.getElementById("far").value;
 
-    if (cel != "") {
+    if (cel !== "") {
         var Fahrenheit = (cel * 1.8) + 32;
         var Kelvin = parseFloat(cel) + 273.15;
         document.getElementById('far').value = parseFloat(Fahrenheit).toFixed(1);
         document.getElementById('kel').value = parseFloat(Kelvin).toFixed(0);
-
-    } else if (far != "") {
+    } else if (far !== "") {
         var Celcius = (far - 32) * (5 / 9);
         var KelvinValue = (far - 32) / 1.8 + 273.15;
         document.getElementById('Celi').value = parseFloat(Celcius).toFixed(1);
         document.getElementById('kel').value = parseFloat(KelvinValue).toFixed(1);
-    } else if (kel != "") {
+    } else if (kel !== "") {
         var CelciusValue = kel - 273;
         var FahrenheitValue = 1.8 * (kel - 273) + 32;
         document.getElementById('far').value = parseFloat(FahrenheitValue).toFixed(1);
         document.getElementById('Celi').value = parseFloat(CelciusValue).toFixed(1);
     }
 }
-Re.addEventListener("click",function() {
-    document.getElementById("kel").value=" ";
-    document.getElementById("Celi").value=" ";
-    document.getElementById("far").value=" ";
+
+let enter = document.querySelectorAll('.box');
+enter.forEach(function (inp) {
+   inp.addEventListener('keypress', function(e) {
+    if(e.key == 'Enter')
+    temperature();
+   })
 })
+
+Re.addEventListener("click", function() {
+    document.getElementById("kel").value = "";
+    document.getElementById("Celi").value = "";
+    document.getElementById("far").value = "";
+});
